050bd2c956cf040a97eced9edbc2500b1e85085e
bassomatteo <matteobasso9@gmail.com>
2016-01-20 08:45:24 +0100

    bello

---
+++ src/eredit/Prodotto.java
@@ -0,0 +1,64 @@
+package eredit;
+
+public class Prodotto {
+	protected String cod = new String();
+	protected String descr = new String();
+	protected double prezzo;
+
+	public Prodotto(String cod, String descr, double prezzo) {
+		this.cod = cod;
+		this.descr = descr;
+		this.prezzo = prezzo;
+	}
+
+	public Prodotto() {
+	}
+
+	public String getCod() {
+		return cod;
+	}
+
+	public void setCod(String cod) {
+		this.cod = cod;
+	}
+
+	public String getDescr() {
+		return descr;
+	}
+
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(int prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public void applicaSconto() {
+		prezzo -= prezzo / 100 * 5;
+	}
+
+	public String toString() {
+		return "Prodotto [cod=" + cod + ", descr=" + descr + ", prezzo=" + prezzo + "]";
+	}
+
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+}
