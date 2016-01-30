bc1ab7be3d390a773953905d40aeb161133f287f
bassoandrea <bassoandrea@Lab06_23.itseinaudi.local>
2016-01-30 09:51:38 +0100

    modifiche

--- src/spesa/ListaSpesa.java
+++ src/spesa/ListaSpesa.java
@@ -1,16 +1,18 @@
 package spesa;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jface.dialogs.MessageDialog;
+
 public class ListaSpesa {
-	
+
 	Alimentare a = new Alimentare();
 	NonAlimentare n = new NonAlimentare();
 	Data d;
 	public double tot = 0;
+	public List<Prodotto> carrello = new ArrayList<Prodotto>();
 
 	public ListaSpesa() {
 	}
@@ -20,17 +22,17 @@ public class ListaSpesa {
 	}
 
 	public double calcolaTotale(boolean Tessera) {
-		if(Tessera == false){
+		if (Tessera == false) {
 			for (int i = 0; i < carrello.size(); i++) {
 				tot += carrello.get(i).getPrezzo();
-			}			
-		}else{
+			}
+		} else {
 			for (int i = 0; i < carrello.size(); i++) {
-				if(carrello.get(i) instanceof Alimentare == true){
+				if (carrello.get(i) instanceof Alimentare == true) {
 					a = (Alimentare) carrello.get(i);
 					a.applicaSconto();
 					tot = tot + a.getPrezzo();
-				}else{
+				} else {
 					n = (NonAlimentare) carrello.get(i);
 					n.applicaSconto();
 					tot = tot + n.getPrezzo();
@@ -45,8 +47,6 @@ public class ListaSpesa {
 		carrello.remove(index);
 	}
 
-	public List<Prodotto> carrello = new ArrayList<Prodotto>();
-
 	public String leggiStringa(BufferedReader br) {
 		try {
 			return br.readLine();
@@ -59,4 +59,58 @@ public class ListaSpesa {
 	public int leggiInt(BufferedReader br) {
 		return Integer.parseInt(leggiStringa(br));
 	}
+
+	public void leggiFile() {
+		BufferedReader lettore;
+		String riga = "";
+		String testo = "";
+		try {
+			lettore = new BufferedReader(new FileReader("prova.txt"));
+			do {
+				testo = testo + riga + "\n";
+				riga = lettore.readLine();
+			} while (riga != null);
+			// MessageDialog.openInformation(shell, "Lettura file", "Riga: " +
+			// testo);
+			System.out.println(testo);
+
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			// MessageDialog.openError(shell, "Lettura file", "ERRORE");
+			System.out.println("ERRORE");
+		}
+
+	}
+
+	public void salvaFile() {
+		BufferedWriter scrittore;
+		try {
+			scrittore = new BufferedWriter(new FileWriter("Scontrino.txt", true));
+
+			for (int i = 0; i < carrello.size(); i++) {
+				Prodotto p = carrello.get(i);
+				try {
+					scrittore.write(p.getDescr() + "\n");
+					scrittore.write(p.getCod() + "\n");
+
+					// MessageDialog.openInformation(shell, "Scrittura file",
+					// "TUTTO OK");
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					// MessageDialog.openError(shell, "Scrittura file",
+					// "ERRORE");
+				}
+			}
+
+			scrittore.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // NB true per APPEND
+
+	}
+
 }
